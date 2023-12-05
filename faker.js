25a9f1506aca075b22fd90cac4f6d4046127fc1c
Lucas Porto <lucasportojpa@gmail.com>
2023-12-05 18:20:25 +0000

    removal of test file

--- faker.js
+++
@@ -1,6 +0,0 @@
-const { faker } = require('@faker-js/faker');
-
-// const randonTest = faker.company.name();
-
-const randomName = faker.lorem.paragraphs(10, '<br/>\n');; // Rowan Nikolaus
-console.log(randomName);
