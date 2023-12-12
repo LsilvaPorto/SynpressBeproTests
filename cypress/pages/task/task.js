b9cea4602d1442bd86cf721300dc35ca2e8c58ab
Lucas Porto <lucasportojpa@gmail.com>
2023-12-12 14:08:56 +0000

    Create deliverable flow complete

--- cypress/pages/task/task.js
+++ cypress/pages/task/task.js
@@ -6,19 +6,17 @@ export default class TaskPage extends Page {
         statusDeliverableComponent: '#root-container > div.mt-3.pb-2.border-bottom.border-gray-850 > div > div > div > div > div:nth-child(2) > div.row.d-flex.flex-wrap.justify-content-between > div.col.d-flex.flex-wrap.align-items-center.mt-3 > div.my-2 > div',
         btn: 'button',
         btnStartWorking: '#root-container > div.container-xl > div > div > div.mt-4 > div > div > div > div > div > button',
-        btnCreateDeliverable: '#root-container > div.container-xl > div > div > div.mt-4 > div > div > div > div > div > div > button:contains("Create Deliverable")',
         inputDeliverableLink: ':nth-child(1) > .col-md-12 > .form-group > .form-control',
         inputDeliverableTitle: ':nth-child(3) > .col-md-12 > .form-group > .form-control',
         inputDeliverableDescription: '.p-1 > .form-control',
-        btnCreateDeliverable: '.d-none.d-flex > .ps-3 > .btn',
+        btnToFinishDeliverableCreation: '.d-none.d-flex > .ps-3 > .btn',
         btnMarkAsReady: '#make-ready-for-review-modal > div > div.row.mx-0.modal-footer > div > button.btn.btn-primary.text-white.d-flex.align-items-center.justify-content-center.text-uppercase.shadow-none:contains("Mark as ready")',
         imgPreviewLinkDeliverable: 'img[src="https://afrodite.bepro.network/images/meta-thumbnail.jpeg"].border-radius-8',
 
     }
     link = 'https://afrodite.bepro.network';
     waitTaskChangeStatusToOpen() {
-        cy.wait(60000);
-        cy.reload();
+        cy.wait(30000);
         cy.waitUntil(() => {
             // Use cy.get dentro do loop para obter o elemento atualizado
             return cy.get(this.locator.statusTaskComponent)
@@ -30,14 +28,18 @@ export default class TaskPage extends Page {
                     });
             
                 });
-        }, { timeout: 120000, interval: 5000 })
+        }, { timeout: 120000, interval: 5000 }).then(() => {
+            console.log('Task status changed to open');
+            cy.wait(1000);
+        })
     }
 
     createDeliverable() {
         this.waitTaskChangeStatusToOpen();
         // Encontre o botão pelo seu texto
         cy.get(this.locator.btn).invoke('text').then(($buttonText) => {
-            if ($buttonText === 'Start Working') {
+            cy.log($buttonText);
+            if ($buttonText.includes('Start Working')) {
                 // O botão "Start Working" está presente, clique nele
                 cy.get(this.locator.btnStartWorking).click({ force: true });
 
@@ -54,7 +56,7 @@ export default class TaskPage extends Page {
         cy.get(this.locator.imgPreviewLinkDeliverable);
         cy.get(this.locator.inputDeliverableTitle).type(this.createTaskTitle(), { force: true });
         cy.get(this.locator.inputDeliverableDescription).type(this.createTaskDescription(), { force: true });
-        cy.get(this.locator.btnCreateDeliverable).wait(1000).click();
+        cy.get(this.locator.btnToFinishDeliverableCreation).wait(1000).click();
         cy.confirmMetamaskTransaction({ timeout: 60000 });
         cy.get(this.locator.btnMarkAsReady).wait(1000).click();
         cy.confirmMetamaskTransaction();
